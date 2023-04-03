8ae72cb1159c6f84fadc00d0732592a5cbdf1f7b
Farhan Azam <40482801+farhanazam98@users.noreply.github.com>
2023-04-03 08:46:44 -0700

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, toRemove) {
+    s = new Set();
+    for (var i = 1; i < arguments.length; i++) {
+        s.add(arguments[i]);
+    }
+    newArray = [];
+    for (item of arr){
+        if (!s.has(item)){
+            newArray.push(item);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line

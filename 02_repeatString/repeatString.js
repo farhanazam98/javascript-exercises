a20c91911af4751d3e1acdcc139b630367a3671d
Farhan Azam <40482801+farhanazam98@users.noreply.github.com>
2023-04-02 21:34:24 -0700

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+    if (n < 0){
+        return "ERROR";
+    }
+    let finalString = '';
+    for (let i = 0; i < n; i++){
+        finalString += s;
+    }
+    return finalString
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;

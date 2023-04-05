c9592f4ee61ec1fe6b9298cbb7157821045ff316
Farhan Azam <40482801+farhanazam98@users.noreply.github.com>
2023-04-04 19:15:37 -0700

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+  return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+  return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (item of arr){
+    sum += item;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  product = 1;
+  arr.forEach(element => {
+    product *= element;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let result = 1;
+  for (let i = n; i > 0; i--){
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line

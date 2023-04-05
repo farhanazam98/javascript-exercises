037825be45410234448cf04e8340f3a0d7b38253
Farhan Azam <40482801+farhanazam98@users.noreply.github.com>
2023-04-04 20:06:08 -0700

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    word = word.toLowerCase();
+    word = removeInvalidChars(word);
+    let left = 0;
+    let right = word.length - 1;
+    while (left < right){
+        if (word.charAt(left) != word.charAt(right)) return false;
+        left++;
+        right--;
+    }
+    return true;
 };
+
+function removeInvalidChars(word){
+    res = "";
+    for (let i = 0; i < word.length; i++){
+        c = word.charAt(i);
+        if ('a' <= c && c <= 'z') res += c;
+    }
+    return res;
+}
 
 // Do not edit below this line
 module.exports = palindromes;

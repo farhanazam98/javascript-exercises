8e77c8906a6845a3e002bd7b49336ca5a0ad95d9
Farhan Azam <40482801+farhanazam98@users.noreply.github.com>
2023-04-04 20:47:56 -0700

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (typeof n == "string") n = Number(n);
+    if (!isValid(n)) return "OOPS";
+    if (n <= 1){
+        return n;
+    }
+    nMinus1 = 1;
+    nMinus2 = 0;
+    for (let i = 2; i <= n; i++){
+        tmp = nMinus1;
+        nMinus1 = nMinus1 + nMinus2;
+        nMinus2 = tmp;
+    }
+    return nMinus1;
 };
+
+function isValid(n){
+    return n > 0;
+}
 
 // Do not edit below this line
 module.exports = fibonacci;

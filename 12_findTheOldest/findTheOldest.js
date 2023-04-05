86ce78d033bf9d281dabc36b0fd8dee5c3415bed
Farhan Azam <40482801+farhanazam98@users.noreply.github.com>
2023-04-04 21:04:05 -0700

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestPerson;
+    let oldestAge = 0;
+    const currentYear = 2023;
+    people.forEach(person => {
+        let age;
+        if (person.yearOfDeath == null){
+            age = currentYear - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        if (age > oldestAge){
+            oldestPerson = person;
+            oldestAge = age;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line

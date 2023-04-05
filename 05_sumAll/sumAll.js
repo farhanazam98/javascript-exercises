00c7b07d6f58ad76dd1daff91eb5f6e341553d93
Farhan Azam <40482801+farhanazam98@users.noreply.github.com>
2023-04-04 18:40:48 -0700

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(num1 , num2) {
+    if (!isValid(num1) || !isValid(num2)){
+        return "ERROR";
+    }
+    let sum = 0;
+    if (num1 < num2){
+        for (let i = num1; i <= num2; i++){
+            sum += i;
+        }
+    } else {
+        for (let i = num2; i <= num1; i++){
+            sum += i;
+        }
+    }
+    return sum
 };
+
+function isValid(num){
+    if (typeof num === "number" && num >= 0){
+        return true;
+    }
+    return false;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
